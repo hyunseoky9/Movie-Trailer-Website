c54f58ba2dd9f46349da7413056d3247ed076993
jin00105 <hy324@cornell.edu>
2017-09-03 17:03:59 -0700

    hey

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-#Movie Trailer Website
+# Movie Trailer Website
 
 This program renders the webpage that has 3 best movies of all time: _The Avengers_, _Iron Man_, and _Beautiful Mind_. The website will have 3 poster images of each of the 3 movies with title undeneath, and if you click on one of them, it will play a youtube trailer. Enjoy!
 
-#How to Use
+# How to Use
 
 Download or clone both files (media.py and fresh_tomatoes.py), and run media.py module.
 It will then open up a web browser with the specified movie web page!
--- media.py
+++ media.py
@@ -13,27 +13,27 @@ class Movie():
             trailer: string of youtube trailer url
     """
     def __init__(self,title,image,trailer):
-    """ 
-        Inits Movie with title, image, and trailer information
-    """    
-    self.title = title
-    self.poster_image_url = image
-    self.trailer_youtube_url = trailer
+        """ 
+            Inits Movie with title, image, and trailer information
+        """    
+        self.title = title
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
 
     def show_trailer(self):
-    """
-        Opens a webbrowser with youtube trailer that's specified in the trailer_youtube_url
-    """
-    webbrowser.open(self.trailer_url)
+        """
+            Opens a webbrowser with youtube trailer that's specified in the trailer_youtube_url
+        """
+        webbrowser.open(self.trailer_url)
 #three objects created by Movie class.
 Avengers = Movie('The Avengers',
-    'https://en.wikipedia.org/wiki/The_Avengers_(2012_film)#/media/File:TheAvengers2012Poster.jpg',
+    'https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg',
     'https://www.youtube.com/watch?v=eOrNdBpGMv8')
 Iron_man = Movie('Iron Man',
-    'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=0ahUKEwism92t_YnWAhVB4GMKHbYdCZkQjRwIBw&url=http%3A%2F%2Fgoogle.com%2Fsearch%3Ftbm%3Disch%26q%3DIron%2520Man&psig=AFQjCNFhfzh5Tz1fe2eDe7aW0SmX683XGA&ust=1504561194332529',
+    'https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG',
     'https://www.youtube.com/watch?v=8hYlB38asDY')
 Beautiful_mind = Movie('Beautiful Mind',
-    'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=0ahUKEwjT9MXE_YnWAhVW-GMKHXUnAeAQjRwIBw&url=http%3A%2F%2Fgoogle.com%2Fsearch%3Ftbm%3Disch%26q%3DA%2520Beautiful%2520Mind&psig=AFQjCNHItC7ryxp-f1SVs3fs8pUHbeCF2w&ust=1504561242330094',
+    'https://upload.wikimedia.org/wikipedia/en/b/b8/A_Beautiful_Mind_Poster.jpg',
     'https://www.youtube.com/watch?v=YWwAOutgWBQ')
 
 movies = [Avengers, Iron_man, Beautiful_mind]
